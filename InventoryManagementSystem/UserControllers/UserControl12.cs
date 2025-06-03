7692b01d84e14b0d32a63f761b2af5e719d7d271
MohabWafaie <mohab.wafaie@gmail.com>
2025-06-03 22:04:31 +0300

    added controller

--- InventoryManagementSystem/UserControllers/UserControl12.cs
+++ InventoryManagementSystem/UserControllers/UserControl12.cs
@@ -7,14 +7,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using InventoryManagementSystem.Controllers;
 
 namespace InventoryManagementSystem.UserControllers
 {
     public partial class UserControl12 : UserControl
     {
+        ProductInWarehouseController pwc;
+        WithdrawPermitController wpc;
+        WithdrawPermitProductController wppc;
+        ClientController cc;
+        WarehouseController wc;
+        ProductController productController;
         public UserControl12()
         {
             InitializeComponent();
+            pwc = new ProductInWarehouseController();
+            wpc = new WithdrawPermitController();
+            wppc = new WithdrawPermitProductController();
+            cc = new ClientController();
+            wc = new WarehouseController();
+            productController = new ProductController();
         }
 
         private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
